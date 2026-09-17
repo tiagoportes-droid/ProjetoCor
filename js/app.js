006d3bb2c30b8af05bd079662513a3290bc91bda
TIAGO FERREIRA DA LUZ PORTES <26171637@edu119.local>
2026-09-17 09:24:13 -0300

    style: more styl e in buttons

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 const botao = document.getElementById("botao");
-const resposta = document.getElementById("resp")
+const resposta = document.getElementById("resp");
 
 const ola = function () {
   resposta.textContent = "Olá, seja Bem-vindo(a)";
 };
 
-const soma = (n1, n2) => {
-    const n1 = 10;
-    const n2 = 5
-    const resu = n1 + n2;
-    resu = resu.toSting();
-    resposta.innerHTML = resu
 
-}
-botao.addEventListener('click', soma)
+setTimeout(() => {
+  const soma = () => {
+    const n1 = 10;
+    const n2 = 5;
+    let resu = n1 + n2;
+    resu = resu.toString();
+    resposta.innerHTML = resu;
+  };
+
+  // O clique só é registrado aqui dentro
+  botao.addEventListener("click", soma);
+}, 1500);
